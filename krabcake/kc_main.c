98e495196481a188b1fe6a375b206d14c3fdf541
Felix S. Klock II <pnkfelix@pnkfx.org>
2023-03-27 22:34:15 -0400

    checkpoint skeletal kc_instrument code.

--- krabcake/kc_main.c
+++ krabcake/kc_main.c
@@ -33,6 +33,11 @@
 
 #include "krabcake.h" /* for client requests */
 
+/* For code instrumentation */
+#include "libvex_basictypes.h"
+#include "libvex_ir.h"
+#include "libvex.h"
+
 #include <stdint.h>
 
 // declare the Rust function
@@ -110,13 +115,122 @@ static void kc_post_clo_init(void)
 
 static
 IRSB* kc_instrument ( VgCallbackClosure* closure,
-                      IRSB* bb,
+                      IRSB* sbIn,
                       const VexGuestLayout* layout,
                       const VexGuestExtents* vge,
                       const VexArchInfo* archinfo_host,
                       IRType gWordTy, IRType hWordTy )
 {
-   return bb;
+   IRSB*      sbOut;
+   IRTypeEnv* tyenv = sbIn->tyenv;
+
+   /* Set up SB. As this says, it copies everything except the statement list.
+      That means it copies the type enviroment and the basic-block control-flow
+      structure.
+    */
+   sbOut = deepCopyIRSBExceptStmts(sbIn);
+
+   Int      i;
+   IRStmt** sts2;
+
+   for (i = 0; i < sbIn->stmts_used; i++) {
+      IRStmt* st = sbIn->stmts[i];
+      if (!st) continue;
+      switch (st->tag) {
+         // == LOAD+STORE instructions ==
+
+         // `t<tmp> = <data>` assigns value to an (SSA) temporary.
+      case Ist_WrTmp: {
+         IRExpr* data = st->Ist.WrTmp.data;
+         IRType  type = typeOfIRExpr(tyenv, data);
+         if (data->tag == Iex_Load) {
+            /* FIXME */
+         }
+         addStmtToIRSB( sbOut, st );
+         break;
+      }
+         // `ST<end>(<addr>) = <data>` writes value to memory, unconditionally.
+      case Ist_Store: {
+         IRExpr* data = st->Ist.Store.data;
+         IRType  type = typeOfIRExpr(tyenv, data);
+         /* FIXME */
+         addStmtToIRSB( sbOut, st );
+         break;
+      }
+         // `t<tmp> = if (<guard>) <cvt>(LD<env>(<addr>)) else <alt>` guarded load
+      case Ist_LoadG: {
+         IRLoadG* lg       = st->Ist.LoadG.details;
+         IRType   type     = Ity_INVALID; /* loaded type */
+         IRType   typeWide = Ity_INVALID; /* after implicit widening */
+         typeOfIRLoadGOp(lg->cvt, &typeWide, &type);
+         /* FIXME */
+         addStmtToIRSB( sbOut, st );
+         break;
+      }
+         // `if (<guard>) ST<end>(<addr>) = <data>` guarded store; all fields
+         // unconditionally evaluated.
+      case Ist_StoreG: {
+         /* FIXME */
+         addStmtToIRSB( sbOut, st );
+         break;
+      }
+         // `t<tmp> = CAS<end>(<addr> :: <expected> -> <new>)` atomic compare-and-swap
+      case Ist_CAS: {
+         Int    dataSize;
+         IRType dataTy;
+         IRCAS* cas = st->Ist.CAS.details;
+         tl_assert(cas->addr != NULL);
+         tl_assert(cas->dataLo != NULL);
+         dataTy   = typeOfIRExpr(tyenv, cas->dataLo);
+         /* FIXME */
+         addStmtToIRSB( sbOut, st );
+         break;
+      }
+         // `result = LD<end>-Linked(<addr>)` if STOREDATA is null
+         // `result = ( ST<end>-Cond(<addr>)` if STOREDATA nonnull
+      case Ist_LLSC: {
+         IRType dataTy;
+         if (st->Ist.LLSC.storedata == NULL) {
+            /* LL */
+            dataTy = typeOfIRTemp(tyenv, st->Ist.LLSC.result);
+            /* FIXME */
+         } else {
+            /* SC */
+            dataTy = typeOfIRExpr(tyenv, st->Ist.LLSC.storedata);
+            /* FIXME */
+         }
+         addStmtToIRSB( sbOut, st );
+         break;
+      }
+
+         // == REMAINING INSTRUCTIONS NOT YET MONITORED BY KRABCAKE ==
+
+         // `IR-NoOp`, can be included or omitted without effect.
+      case Ist_NoOp:
+         // `IMark(<addr>, <len>, <delta>)` marks start of statements
+         // representing single machine instruction.
+      case Ist_IMark:
+         // `AbiHint(<base>, <len>, <nia>)` says something about platform's ABI
+      case Ist_AbiHint:
+         // `PUT(<offset>) = <data>` writes guest register at fixed offset
+      case Ist_Put:
+         // `PUTI<descr>[<ix>,<bias>] = <data>` writes guest register at
+         // non-fixed offset
+      case Ist_PutI:
+         // `ty = DIRTY <guard> <effects> ::: <callee>(<args>)` conditional call
+         // to side-effecting C function.
+      case Ist_Dirty:
+         // `MBusEvent-Fence`,`MBusEvent-BusLock`, `MBusEvent-BusUnlock`
+      case Ist_MBE:
+         // `if (<guard>) goto { <jk> } <dst>` conditional exit from mid-IRSB.
+      case Ist_Exit: {
+            addStmtToIRSB( sbOut, st );
+            break;
+      }
+      }
+   }
+
+   return sbOut;
 }
 
 static void kc_fini(Int exitcode)
